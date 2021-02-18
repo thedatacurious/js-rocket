dd720c34ea1ab756d8ef8d58f24a39e5fff0293a
Alexandra <thedatacurious@gmail.com>
2021-02-18 19:28:50 +0800

    Created rocket animation

---
+++ js/script.js
@@ -0,0 +1,54 @@
+console.log("hello there friends")
+
+// var car = {
+//   color: "red",
+//   isTurnedOn: true,
+//   seats: [
+//     'seat 1',
+//     'seat 2',
+//     'seat 3',
+//     'seat 4',
+//   ],
+//   switchCar: function(isOn){
+//     if (isOn == true){
+//       this.isTurnedOn = true;
+//     } else {
+//       this.isTurnedOn = false;
+//     }
+//   },
+//   fly: function(){
+//     alert('fly');
+//   }
+// }
+
+var timer = null;
+// var countdownNum = 10
+
+
+let changeState = function(state){
+  clearInterval(timer);
+  var countdownNum = 10;
+  document.getElementById('countdown').innerHTML = countdownNum;
+  document.body.className = 'body-state'+ state;
+
+  if (state == 2){
+          timer = setInterval(function(){
+          countdownNum -= 1;
+          document.getElementById('countdown').innerHTML = countdownNum;
+          if (countdownNum <=0){
+            changeState(3);
+          }
+    }, 600);
+  }
+ else if (state == 3){
+   let success = setTimeout(function(){
+     var randomNum = Math.round(Math.random()*10);
+     console.log('random number:', randomNum)
+
+     if (randomNum >= 5){
+       changeState(4);
+     }
+     else {changeState(5);}
+     }, 2000);
+ }
+}
